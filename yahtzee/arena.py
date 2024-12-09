0183549fe9fd6e5221549d5373d56a5453b70a66
jorgenwh <jorgenwhen@gmail.com>
2024-12-09 01:48:31 +0100

    readme

--- yahtzee/arena.py
+++ yahtzee/arena.py
@@ -1,3 +1,4 @@
+import random
 from typing import List, Type
 from matplotlib import pyplot as plt
 
@@ -34,20 +35,32 @@ class Arena():
     def graph_results(self, output_filename: str) -> None:
         agent_names = [result.agent_name for result in self.evaluation_results]
         mean_scores = [result.mean for result in self.evaluation_results]
-        colors = [["blue", "green", "red", "cyan"][i % 4] for i in range(len(agent_names))]
+        colors = ["pink", "green", "orange", "cyan", "red", "olive"]
+        random.shuffle(colors)
+        colors = [colors[i % 6] for i in range(len(agent_names))]
 
         _, ax = plt.subplots(figsize=(9, 9))
         bars = ax.bar(agent_names, mean_scores, color=colors)
 
+        # Make edge colors of the axes to white
+        ax.spines['bottom'].set_color('white')
+        ax.spines['top'].set_color('white')
+        ax.spines['right'].set_color('white')
+        ax.spines['left'].set_color('white')
+
+        # Make tick labels white
+        ax.tick_params(axis='x', colors='white')
+        ax.tick_params(axis='y', colors='white')
+
         for bar, score in zip(bars, mean_scores):
             x = bar.get_x() + bar.get_width() / 2
             y = bar.get_height()
-            ax.text(x, y, f"{score:.1f}", ha="center", va="bottom")
+            ax.text(x, y, f"{score:.1f}", ha="center", va="bottom", color="white")
 
         # Add a line indicating the theoretical maximum score
         plt.axhline(
                 y=MAXIMUM_THEORETICAL_SCORE, 
-                color="black", 
+                color="white", 
                 linestyle="--", 
                 label="Theoretical maximum score"
         )
@@ -55,19 +68,27 @@ class Arena():
         # Add a line indicating the mean score of random play
         plt.axhline(
                 y=EXPECTED_RANDOM_PLAY_SCORE, 
-                color="purple", 
+                color="cyan", 
                 linestyle="--", 
                 label="Random play"
         )
 
-        plt.title(f"mean agent scores after {self.num_episodes} episodes")
-        plt.xlabel("agent")
-        plt.ylabel("mean score")
+        plt.title(f"mean agent scores after {self.num_episodes} episodes", color="white")
+        plt.xlabel("agent", color="white")
+        plt.ylabel("mean score", color="white")
 
-        plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.1), ncol=3)
+        plt.legend(
+                loc="upper center", 
+                bbox_to_anchor=(0.5, -0.1), 
+                ncol=3,
+                frameon=False,
+        )
+        for text in ax.get_legend().get_texts():
+            text.set_color("white")
         plt.subplots_adjust(bottom=0.2)
 
-        # set background color
-        ax.set_facecolor("lightgrey")
+        # set background color of image and plot
+        plt.gcf().set_facecolor((0.1, 0.1, 0.2))
+        ax.set_facecolor((0.1, 0.1, 0.2))
 
         plt.savefig(output_filename)
