20cdb71a6652ef76f807118bb37829a6c4772d68
jorgenwh <jorgenwhen@gmail.com>
2024-12-13 17:10:18 +0100

    put rollout number for mcts in a constant

--- yahtzee/agents/mcts_agent/mcts_agent.py
+++ yahtzee/agents/mcts_agent/mcts_agent.py
@@ -4,14 +4,17 @@ from yahtzee.yahtzee import State
 from .mcts import MonteCarloTreeSearch
 
 
+MCTS_ROLLOUTS = 5000
+
+
 class MctsAgent(Agent):
     def __init__(self):
         self.mcts = MonteCarloTreeSearch()
 
     def get_action(self, state: State) -> int:
         self.mcts = MonteCarloTreeSearch()
-        action = self.mcts.search(state, 3000)
+        action = self.mcts.search(state, MCTS_ROLLOUTS)
         return action
 
     def get_name(self) -> str:
-        return "MCTS Agent"
+        return "MCTS Agent [" + str(MCTS_ROLLOUTS) + " rollouts]"
